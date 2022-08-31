aa3e290e16fe09db4a147b9b41161ee8a85134a2
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-08-30 22:43:25 -0500

    feat: creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskComponent } from './components/task/task.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    TaskFormComponent,
+    TaskComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule
